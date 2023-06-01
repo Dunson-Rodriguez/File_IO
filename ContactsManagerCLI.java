129b440aafd5764780fab7e1062e6f7d590e7f3e
Jakira Dunson <jakiradunson@gmail.com>
2023-06-01 14:17:00 -0500

    new rep

---
+++ ContactsManagerCLI.java
@@ -0,0 +1,162 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ContactsManagerCLI {
+
+    private static final String CONTACTS_FILE = "contacts.txt";
+    public static void main(String[] args) {
+
+        List<Contact> contacts = loadContacts();
+
+        while (true) {
+            System.out.println("""
+            \nMain Menu:
+           1. View contacts.
+           2. Add a new contact.
+           3. Search a contact by name.
+           4. Delete an existing contact.
+           5. Exit.
+
+           Enter an option (1, 2, 3, 4, or 5): """);
+
+
+
+            Scanner scanner = new Scanner(System.in);
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    showContacts(contacts);
+                    break;
+                case "2":
+                    addContact(contacts);
+                    break;
+                case "3":
+                    searchContact(contacts);
+                    break;
+                case "4":
+                    deleteContact(contacts);
+                    break;
+                case "5":
+                    saveContacts(contacts);
+                    System.out.println("Exiting... Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    private static List<Contact> loadContacts() {
+        List<Contact> contacts = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(CONTACTS_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                String name = parts[0];
+                String phoneNumber = parts[1];
+                Contact contact = new Contact(name, phoneNumber);
+                contacts.add(contact);
+            }
+        } catch (IOException e) {
+            // Ignore if file does not exist or error occurs during reading
+        }
+
+        return contacts;
+    }
+
+    private static void saveContacts(List<Contact> contacts) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(CONTACTS_FILE))) {
+            for (Contact contact : contacts) {
+                writer.write(contact.getName() + "|" + contact.getPhoneNumber());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred while saving contacts: " + e.getMessage());
+        }
+    }
+
+    private static void showContacts(List<Contact> contacts) {
+        System.out.println("Name\t\tPhone number");
+        System.out.println("-".repeat(30));
+        for (Contact contact : contacts) {
+            System.out.println(contact.getName() + "\t" + contact.getPhoneNumber());
+        }
+    }
+
+    private static void addContact(List<Contact> contacts) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the contact's name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter the contact's phone number: ");
+        String phoneNumber = scanner.nextLine();
+
+        Contact contact = new Contact(name, phoneNumber);
+        contacts.add(contact);
+
+        System.out.println("Contact added successfully!");
+    }
+
+    private static void searchContact(List<Contact> contacts) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the contact's name to search: ");
+        String name = scanner.nextLine();
+
+        for (Contact contact : contacts) {
+            if (contact.getName().equalsIgnoreCase(name)) {
+                System.out.println("Contact found:");
+                System.out.println("Name: " + contact.getName());
+                System.out.println("Phone number: " + contact.getPhoneNumber());
+                return;
+            }
+        }
+
+        System.out.println("Contact not found!");
+    }
+
+    private static void deleteContact(List<Contact> contacts) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the contact's name to delete: ");
+        String name = scanner.nextLine();
+
+        Contact foundContact = null;
+        for (Contact contact : contacts) {
+            if (contact.getName().equalsIgnoreCase(name)) {
+                foundContact = contact;
+                break;
+            }
+        }
+
+        if (foundContact != null) {
+            contacts.remove(foundContact);
+            System.out.println("Contact deleted successfully!");
+        } else {
+            System.out.println("Contact not found!");
+        }
+    }
+}
+
+class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
