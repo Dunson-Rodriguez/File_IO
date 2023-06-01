92c8753d821c02b35f90e038158a5e3193736198
Jakira Dunson <jakiradunson@gmail.com>
2023-06-01 14:56:40 -0500

    contact list

---
+++ ContactList.java
@@ -0,0 +1,8 @@
+public class ContactList {
+    public static ContactList[] findAll() {
+        return new Movie[]{
+                new Movie("Citizen Kane", "drama"),
+                new Movie("Casablanca", "drama"),
+        };
+    }
+}
